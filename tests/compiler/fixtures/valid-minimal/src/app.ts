df38c749575b5b531d4a871f759a4305a662284a
Mustafa Elsayed <mustafaskyer@gmail.com>
2026-08-26 10:14:04 +0300

    test(compiler): move minimal fixture application to config

--- tests/compiler/fixtures/valid-minimal/src/app.ts
+++
@@ -1,4 +0,0 @@
-import { defineApp, defineEnv, env as envFactory } from "@zsys/app";
-
-const env = defineEnv({ SERVICE_PORT: envFactory.port().default(3000) });
-export default defineApp({ id: "minimal-app", env, providers: {} });
