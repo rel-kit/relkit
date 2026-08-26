c1dca2b34765fce927403b43667c449aeb5f7a22
Mustafa Elsayed <mustafaskyer@gmail.com>
2026-08-26 10:13:41 +0300

    refactor(template-minimal): move app declaration to config

--- templates/default/v1/minimal/src/app.ts
+++
@@ -1,9 +0,0 @@
-import { defineApp } from "@zsys/app";
-import env from "./env.js";
-
-export default defineApp({
-  id: "my-app",
-  env,
-  providers: {},
-  observability: { bodyCapture: { mode: "off" } },
-});
