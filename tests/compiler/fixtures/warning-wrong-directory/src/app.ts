1b847ddeb554caf4acb46198e42e1334fad546b2
Mustafa Elsayed <mustafaskyer@gmail.com>
2026-08-26 10:14:04 +0300

    test(compiler): update wrong-directory fixture graph

--- tests/compiler/fixtures/warning-wrong-directory/src/app.ts
+++
@@ -1,4 +0,0 @@
-import { defineApp, defineEnv, env as envFactory } from "@zsys/app";
-
-const env = defineEnv({ SERVICE_PORT: envFactory.port().default(3000) });
-export default defineApp({ id: "warning-directory-app", env, providers: {} });
