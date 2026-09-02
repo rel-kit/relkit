30f6c1aecde2716da7c14b51b211818b6ed67f67
Mustafa Elsayed <mustafaskyer@gmail.com>
2026-09-02 22:52:25 +0300

    feat(commerce): configure integration profiles

--- examples/commerce/.relkit/generated/event-registry.d.ts
+++ examples/commerce/.relkit/generated/event-registry.d.ts
@@ -1,9 +1,6 @@
 declare global {
   namespace Relkit {
     interface EventRegistry {
-    readonly "orders.cancelled": typeof import("../../src/orders/events/order-cancelled.event.js")["default"];
-    readonly "orders.created": typeof import("../../src/orders/events/order-created.event.js")["default"];
-    readonly "orders.updated": typeof import("../../src/orders/events/order-updated.event.js")["default"];
     }
   }
 }
