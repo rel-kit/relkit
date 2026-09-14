e875c815be79afcb736c4aabd8998bf80ffc1057
Mustafa Elsayed <mustafaskyer@gmail.com>
2026-09-14 14:11:48 +0300

    feat(app): expose task-first and legacy job entrypoints

--- packages/jobs/src/index.ts
+++ packages/jobs/src/index.ts
@@ -1,2 +1,71 @@
+export * from "./define-task.js";
 export * from "./define-job.js";
-export * from "./client.js";
+export * from "./task-types.js";
+export * from "./task-policy-validation.js";
+export * from "./task-wire.js";
+export * from "./task-progress.js";
+export { assertRfc3339Instant, isRfc3339Instant } from "./instant-validation.js";
+export { copyTriggerOptions, validateResultOptions } from "./trigger-validation.js";
+export type {
+  JobAccessGrant,
+  JobAccessRequest,
+  JobClientOperation,
+  JobErrorEnvelope,
+  JobRunStatus,
+  JobUnknownOutcome,
+  JsonWireEnvelope,
+  JobWireEnvelope,
+  NamedStreamFrame,
+  ProgressEmitReceipt,
+  ResultAvailability,
+  RunAvailability,
+  RunCancellationReceipt,
+  RunConnection,
+  RunHandle,
+  RunListQuery,
+  RunPage,
+  RunRetryReceipt,
+  RunSnapshot,
+  RunWatchFrame,
+  StreamIdentity,
+  StreamItem,
+  VoidWireEnvelope,
+} from "@relkit/contracts/jobs";
+export type {
+  DefineJobOptions,
+  JobAdmission,
+  JobClientAccess,
+  JobClientField,
+  JobDescriptor,
+  JobDescriptorAny,
+  JobScheduleClient,
+  JobRunResultOptions,
+  JobServiceSelection,
+  ScheduleDefinition,
+  ScheduleMisfire,
+  ScheduleOverlap,
+  ScheduleReadReceipt,
+  ScheduleRecord,
+  ScheduleListOptions,
+  ScheduleWriteOptions,
+  ScheduleWriteOutcome,
+  ScheduleWriteReceipt,
+} from "./job-types.js";
+export {
+  durationToMillis,
+  isDurationInput,
+  parseDuration,
+  validateDuration,
+} from "./duration.js";
+export type { DurationInput, DurationUnit } from "./duration.js";
+export {
+  assertJobName,
+  isJobName,
+  isValidJobName,
+  normalizeJobName,
+  validateJobName,
+  JOB_NAME_PATTERN,
+  JOB_NAME_RESERVED,
+} from "./job-name.js";
+export type { JobName, ValidJobName } from "./job-name.js";
+export { JOB_NAME_MAX_LENGTH } from "@relkit/contracts/jobs";
